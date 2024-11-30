d587e05942b5a2475837161033cacbf64dfa2309
mohourzik <our.mohand@gmail.com>
2024-11-30 10:30:51 +0100

    concat the train-test-valid file to get a single dataset

---
+++ prep.py
@@ -0,0 +1,18 @@
+train_data = 'data/train.csv'
+test_data = 'data/test.csv'
+valid_data = 'data/test.csv'
+
+file = "multiclass_dataset.csv"
+
+def save_load_df(file:str):
+    if os.path.exists(file):
+        df = pd.read_csv(file, index_col= 0)
+    else:
+        df = pd.concat(map(pd.read_csv, [train_data, test_data, valid_data]), axis= 0, ignore_index=True)
+        df.to_csv(file, columns= ['id', 'text', 'label', 'sentiment'])
+        df = pd.read_csv(file, index_col= 0)
+
+    return df
+
+df = save_load_df(file=file)
+df
